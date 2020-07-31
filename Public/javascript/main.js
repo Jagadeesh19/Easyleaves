27ef3ab2f7ab8a0c42689e4e5abae9dc3180ab24
jagadeesh <yeturujagadeesh@gmail.com>
2020-07-31 15:35:00 +0530

    made auth homepage and navbar responsive

---
+++ Public/javascript/main.js
@@ -0,0 +1,13 @@
+const toggleButton=document.querySelector(".toggle-button");
+const mobileNav=document.querySelector(".mobile-nav");
+const backdrop=document.querySelector(".backdrop");
+
+toggleButton.addEventListener("click",()=>{
+   mobileNav.style.display="block";
+   backdrop.style.display="block";
+});
+
+backdrop.addEventListener("click",()=>{
+   mobileNav.style.display="none";
+   backdrop.style.display="none";
+})
